1b1b5f553b9ec3ea7c4e52dcafad9a9cb667486d
Ninjansh <142496574+ninjansh@users.noreply.github.com>
2024-03-16 22:43:20 +0530

    Create Fabonnacci series (while loop).c

---
+++ Fabonnacci series (while loop).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{
+ int n,fact=1;
+  print("Enter the number upto your desire : ");
+  scanf(%d,&n);
+   while(n>1)
+   {
+     fact*=n--;
+   }
+  scanf(fact);
+  return 0;
+}
