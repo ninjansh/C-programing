57d59b50733854edd88f2cdd72d4222d94eccf90
ninjansh <142496574+ninjansh@users.noreply.github.com>
2024-03-16 21:48:02 +0530

    Create Square a number.c
    Square a number inputted by the user

---
+++ Square a number.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+  int n,sq;
+  printf("\n Enter the number you want to squared :- ");
+  scanf("%d",&n);
+  sq=n*n;
+  printf("\n The square of given number is :- %d",sq);
+  return 0;
+}
